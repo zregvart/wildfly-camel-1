dba7bc4190730570a7569f0318c1d14c99d3058e
Thomas Diesler <thomas.diesler@jboss.com>
2017-02-01 16:23:21 +0100

    [Ignore #1606] Cannot compile optaplanner rules on wildfly-11.0.x

--- itests/standalone/basic/src/test/java/org/wildfly/camel/test/optaplanner/OptaPlannerIntegrationTest.java
+++ itests/standalone/basic/src/test/java/org/wildfly/camel/test/optaplanner/OptaPlannerIntegrationTest.java
@@ -27,6 +27,7 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.optaplanner.examples.cloudbalancing.domain.CloudBalance;
@@ -36,6 +37,7 @@ import org.wildfly.extension.camel.CamelAware;
 
 @CamelAware
 @RunWith(Arquillian.class)
+@Ignore("[#1606] Cannot compile optaplanner rules on wildfly-11.0.x")
 public class OptaPlannerIntegrationTest  {
 
     @Deployment
